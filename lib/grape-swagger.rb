6a27584df6901b58b1bb4f6285abfab7085b2a61
Zach McCormick <zach.mccormick@braze.com>
2026-04-02 12:05:55 -0400

    Add AdditionalPropertiesHandler for property validation keywords
    Handles additionalProperties (both versions), unevaluatedProperties
    (3.1.0 only), and patternProperties (native for 3.1.0, x-extension
    for Swagger 2.0). Only applies to object-type schemas.

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -40,6 +40,7 @@ require 'grape-swagger/openapi/polymorphic_schema_builder'
 require 'grape-swagger/openapi/discriminator_transformer'
 require 'grape-swagger/openapi/conditional_schema_builder'
 require 'grape-swagger/openapi/dependent_schema_handler'
+require 'grape-swagger/openapi/additional_properties_handler'
 require 'grape-swagger/openapi/spec_builder_v3_1'
 
 module GrapeSwagger
